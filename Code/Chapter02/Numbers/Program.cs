909b806db7a85e9fcce583ba21c609b2cb550aaf
AlwaysFocus <eliwhalen3@gmail.com>
2020-12-20 12:06:42 -0700

    Algorithms
    Building algorithms

--- Code/Chapter02/Numbers/Program.cs
+++ Code/Chapter02/Numbers/Program.cs
@@ -6,6 +6,11 @@ namespace Numbers
     {
         static void Main(string[] args)
         {
+
+            // TIP: “Use int for whole numbers and double for real numbers that will not be compared to other values. Use decimal for money, 
+            //       CAD drawings, general engineering, and wherever the accuracy of a real number is important.”
+
+
             // unsigned int means positive whole number including 0
             uint naturalNumber = 23;
             // int means negative or positive whole number
@@ -31,6 +36,35 @@ namespace Numbers
             Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers from {int.MinValue:N0} to {int.MaxValue:N0}");
             Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers from {double.MinValue:N0} to {double.MaxValue:N0}");
             Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers from {decimal.MinValue:N0} to {decimal.MaxValue:N0}");
+
+            // Comparing double and decimal types
+            // *Doubles should only be used when accuracy (eg comparing two numbers) is NOT important.*//
+            // Tip - Never compare double values using == as it will lead to failures in comparison as demonstrated below
+            Console.WriteLine("Using doubles:");
+            double a = 0.1;
+            double b = 0.2;
+            if (a + b == 0.3)
+            {
+                Console.WriteLine($"{a} + {b} equals 0.3");
+            }
+            else
+            {
+                // This is the case that will trigger because 0.1 does not exist as a floating point number.
+                Console.WriteLine($"{a} + {b} does NOT equal 0.3");
+            }
+            // Decimal types
+            // the 'M' suffix is used on decimals to indicate a decimal literal value
+            Console.WriteLine("Using decimals:");
+            decimal c = 0.1M;
+            decimal d = 0.2M;
+            if (c + d == 0.3M)
+            {
+                Console.WriteLine($"{c} + {d} equals 0.3");
+            }
+            else
+            {
+                Console.WriteLine($"{c} + {d} does NOT equal 0.3");
+            }
             
         }
     }
---
+++ Code/Chapter02/Variables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Working with variables
+            object height = 1.88; // Storing a double in an object
+            object name = "Eli"; // storing a string in an object
+            Console.WriteLine($"{name} is {height} meters tall.");
+            //int length1 = name.Length; // Gives compiler error because when the object variable type is used, it does not inherit the properties/method of the values type
+            int length2 = ((string)name).Length; // Tells the compiler that this object is a string.
+            Console.WriteLine($"{name} has {length2} characters.");
+
+        }
+    }
+}
